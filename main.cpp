3052f2be3cffafab26a66a522a3fcb560249526e
Laura Goon <goon.laura@gmail.com>
2022-11-14 00:18:35 -0800

    draw table

--- main.cpp
+++ main.cpp
@@ -1,18 +1,36 @@
+#include <cstdlib>
 #include <iostream>
 using namespace std;
 
+
 void draw()
 {
-    system("cls");
-    cout << "Let's play Tic-Tac-Toe" << endl;
+    system("clear");
+    cout << "Let's play Tic-Tac-Toe!\n\n" << endl;
+
+    for (int i = 0; i < 8; i++)
+    {
+        if ((i+1) % 3 == 0)
+        {
+            cout << "-----|-----|-----" << endl;
+        }
+        else
+        {
+            cout << "     |     |     " << endl;
+        }
+    }
+
+    cout << "\n\n";
 }
 
 int main()
 {
-    while (1)
-    {
-        draw();
-    }
+    draw();
+
+    // while (1)
+    // {
+    //     draw();
+    // }
 
     return 0;
 }
