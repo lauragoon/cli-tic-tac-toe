356d417d2f726f3226d7c63d55c15df1c462ff33
Laura Goon <goon.laura@gmail.com>
2022-11-13 23:48:09 -0800

    initial code commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void draw()
+{
+    system("cls");
+    cout << "Let's play Tic-Tac-Toe" << endl;
+}
+
+int main()
+{
+    while (1)
+    {
+        draw();
+    }
+
+    return 0;
+}
